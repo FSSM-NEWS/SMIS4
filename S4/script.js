83273ceeb0947b90c0975091abed2db8f946dd3a
FSSM-NEWS <96381263+FSSM-NEWS@users.noreply.github.com>
2022-03-06 14:08:01 +0100

    Add files via upload

---
+++ S4/script.js
@@ -0,0 +1,23 @@
+    function prog(){
+        window.open("https://ecampus-fssm.uca.ma/course/view.php?id=708");
+    }
+    
+    function analy(){
+        window.open("https://ecampus-fssm.uca.ma/course/view.php?id=715");
+    }
+    
+    function arch(){
+        window.open("https://ecampus-fssm.uca.ma/course/view.php?id=38");
+    }
+    
+    function sys(){
+        window.open("https://ecampus-fssm.uca.ma/course/view.php?id=41");
+    }
+    
+    function electro(){
+        window.open("https://ecampus-fssm.uca.ma/course/view.php?id=294");
+    }
+
+    function struc(){
+        window.open("https://ecampus-fssm.uca.ma/course/view.php?id=40");
+    }
